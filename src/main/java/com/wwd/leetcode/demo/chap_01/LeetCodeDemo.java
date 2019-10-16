d6443730edee7d560741b19191b17b98f7f1582d
wang.wending@centchain.com <wang.wending@centchain.com>
2019-10-16 14:05:46 +0800

    leetcode演示1

--- src/main/java/com/wwd/leetcode/demo/chap_01/LeetCodeDemo.java
+++ src/main/java/com/wwd/leetcode/demo/chap_01/LeetCodeDemo.java
@@ -2,9 +2,8 @@ package com.wwd.leetcode.demo.chap_01;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.math.BigDecimal;
+import java.util.*;
 
 /**
  * @ClassName LeetCodeOne
@@ -288,4 +287,259 @@ public class LeetCodeDemo {
 
         return mdedianNum;
     }
+
+    // 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+    //
+    //示例 1：
+    //
+    //输入: "babad"
+    //输出: "bab"
+    //注意: "aba" 也是一个有效答案。
+    //示例 2：
+    //
+    //输入: "cbbd"
+    //输出: "bb"
+    //
+
+    @Test
+    public void longestPalindrome() {
+        String s = "\"gphyvqruxjmwhonjjrgumxjhfyupajxbjgthzdvrdqmdouuukeaxhjumkmmhdglqrrohydrmbvtuwstgkobyzjjtdtjroqpyusfsbjlusekghtfbdctvgmqzeybnwzlhdnhwzptgkzmujfldoiejmvxnorvbiubfflygrkedyirienybosqzrkbpcfidvkkafftgzwrcitqizelhfsruwmtrgaocjcyxdkovtdennrkmxwpdsxpxuarhgusizmwakrmhdwcgvfljhzcskclgrvvbrkesojyhofwqiwhiupujmkcvlywjtmbncurxxmpdskupyvvweuhbsnanzfioirecfxvmgcpwrpmbhmkdtckhvbxnsbcifhqwjjczfokovpqyjmbywtpaqcfjowxnmtirdsfeujyogbzjnjcmqyzciwjqxxgrxblvqbutqittroqadqlsdzihngpfpjovbkpeveidjpfjktavvwurqrgqdomiibfgqxwybcyovysydxyyymmiuwovnevzsjisdwgkcbsookbarezbhnwyqthcvzyodbcwjptvigcphawzxouixhbpezzirbhvomqhxkfdbokblqmrhhioyqubpyqhjrnwhjxsrodtblqxkhezubprqftrqcyrzwywqrgockioqdmzuqjkpmsyohtlcnesbgzqhkalwixfcgyeqdzhnnlzawrdgskurcxfbekbspupbduxqxjeczpmdvssikbivjhinaopbabrmvscthvoqqbkgekcgyrelxkwoawpbrcbszelnxlyikbulgmlwyffurimlfxurjsbzgddxbgqpcdsuutfiivjbyqzhprdqhahpgenjkbiukurvdwapuewrbehczrtswubthodv\"";
+        System.out.println(longestPalindrome(s));
+    }
+
+    public String longestPalindrome(String s) {
+        String result = "";
+        String temp = "";
+        int length = s.length();
+        int flag = 0;
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j <= length; j++) {
+                temp = s.substring(i, j);
+                if (isPalindrome(temp)) {
+                    if (temp.length() > flag) {
+                        result = s.substring(i, j);
+                        flag = Math.max(temp.length(), flag);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    // 判断是否是回文字符串
+    private boolean isPalindrome(String result) {
+        boolean flag = true;
+        int length = result.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (result.charAt(i) != result.charAt(length - 1 - i)) {
+                flag = false;
+            }
+        }
+        return flag;
+    }
+
+    // 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
+    //
+    //比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
+    //
+    //L   C   I   R
+    //E T O E S I I G
+    //E   D   H   N
+    // 示例 1:
+    //
+    //输入: s = "LEETCODEISHIRING", numRows = 3
+    //输出: "LCIRETOESIIGEDHN"
+    //示例 2:
+    //
+    //输入: s = "LEETCODEISHIRING", numRows = 4
+    //输出: "LDREOEIIECIHNTSG"
+
+    @Test
+    public void convert() {
+        String s = "A";
+        System.out.println(convert(s,1));
+    }
+
+    public String convert(String s, int numRows) {
+        int flag = 2 * numRows - 2;
+        int length = s.length();
+        int temp = 0;
+        // 存放每行的字符
+        List<StringBuilder> list = new ArrayList<>(numRows);
+        if (flag > 0 && numRows > 1) {
+            for (int j = 0; j < numRows; j++) {
+                StringBuilder stringBuilder = new StringBuilder();
+                list.add(stringBuilder);
+            }
+            for (int i = 0; i < length; i++) {
+                temp = i % flag;
+                if (temp >= numRows) {
+                    temp = flag - temp;
+                }
+                StringBuilder stringBuilder = list.get(temp);
+                stringBuilder.append(s.charAt(i));
+            }
+        } else {
+            return s;
+        }
+
+        System.out.println(list);
+        // 拼接字符串
+        StringBuilder builder = new StringBuilder();
+        for (StringBuilder string : list) {
+            builder.append(string.toString());
+        }
+
+        return builder.toString();
+    }
+
+    @Test
+    public void ttt() {
+        System.out.println(6%6);
+        System.out.println(5%6);
+        System.out.println(7%6);
+    }
+
+    @Test
+    public void reverse() {
+        System.out.println(reverse(1534236469));
+    }
+
+    public int reverse(int x) {
+        Long result = 0L;
+        String string  = "";
+        int length = 0;
+        boolean flag = false;
+        StringBuilder builder = new StringBuilder();
+        if (x > Integer.MIN_VALUE && x < Integer.MAX_VALUE) {
+            string  = String.valueOf(x);
+            length = string.length();
+            for (int i = length - 1; i >= 0; i--) {
+                if (i == length  -1 && string.charAt(i) == '0') {
+                    continue;
+                }
+                if ('-' == string.charAt(i)) {
+                    flag = true;
+                    continue;
+                }
+                builder.append(string.charAt(i));
+            }
+            if (builder.length() > 0 ) {
+                result = Long.parseLong(builder.toString());
+            }
+            if (flag) {
+                result = - result;
+            }
+            if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+                return 0;
+            }
+        }
+        return result.intValue();
+    }
+
+    // 请你来实现一个 atoi 函数，使其能将字符串转换成整数。
+    //
+    //首先，该函数会根据需要丢弃无用的开头空格字符，直到寻找到第一个非空格的字符为止。
+    //
+    //当我们寻找到的第一个非空字符为正或者负号时，则将该符号与之后面尽可能多的连续数字组合起来，作为该整数的正负号；假如第一个非空字符是数字，则直接将其与之后连续的数字字符组合起来，形成整数。
+    //
+    //该字符串除了有效的整数部分之后也可能会存在多余的字符，这些字符可以被忽略，它们对于函数不应该造成影响。
+    //
+    //注意：假如该字符串中的第一个非空格字符不是一个有效整数字符、字符串为空或字符串仅包含空白字符时，则你的函数不需要进行转换。
+    //
+    //在任何情况下，若函数不能进行有效的转换时，请返回 0。
+
+    @Test
+    public void myAtoi() {
+        System.out.println(myAtoi("+1 +42"));
+    }
+
+    public int myAtoi(String str) {
+        Long result = 0L;
+        StringBuilder numStr = new StringBuilder();
+        boolean flag = false;
+        boolean flag2 = false;
+        int length = str.length();
+        char c = 0;
+        String temp = "";
+        if (length > 0) {
+            for (int i = 0; i < length; i++) {
+                c = str.charAt(i);
+                if (' ' == c) {
+                    if (flag || flag2 || numStr.length() > 0) {
+                        break;
+                    }
+                    continue;
+                }
+                if (('0' <= c && c <='9')) {
+                    numStr.append(c);
+                    continue;
+                }
+                if ('-' == c) {
+                    if (flag || flag2 || numStr.length() > 0) {
+                        break;
+                    }
+                    flag = true;
+                    continue;
+                }
+                if ('+' == c) {
+                    if (flag || flag2 || numStr.length() > 0) {
+                        break;
+                    }
+                    flag2 = true;
+                    continue;
+                }
+                else {
+                    break;
+                }
+            }
+        }
+
+        if (flag && flag2) {
+            return 0;
+        }
+
+        if (numStr.length() > 0) {
+            int index = 0;
+            temp = numStr.toString();
+            length = temp.length();
+            for (int j = 0; j < length; j++) {
+                if ('0' == temp.charAt(j)) {
+                    continue;
+                } else {
+                    index = j;
+                    break;
+                }
+            }
+            temp = temp.substring(index, length);
+            if (temp.startsWith("0")) {
+                return 0;
+            }
+            if (temp.length() > 11) {
+                result = Long.MAX_VALUE;
+            } else {
+                result = Long.parseLong(numStr.toString());
+            }
+            if (flag) {
+                result = - result;
+            }
+        }
+
+        if (result > Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+        if (result < Integer.MIN_VALUE) {
+            return Integer.MIN_VALUE;
+        }
+
+        return result.intValue();
+    }
+
+
+    @Test
+    public void dd() {
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+    }
+
 }
